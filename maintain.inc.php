b92053fa6243681744626b0cd5b7684d0175f97a
mistic100 <mistic@piwigo.org>
2011-05-10 16:31:12 +0000

    minor bug
    git-svn-id: http://piwigo.org/svn/extensions/CryptograPHP@10840 68402e56-0260-453c-a942-63ccdbb3a9ee

--- maintain.inc.php
+++ maintain.inc.php
@@ -4,7 +4,7 @@ function plugin_install()
   global $conf;
 
   
-  if (!isset($conf['cryptograph_theme']))
+  if (!isset($conf['cryptographp_theme']))
   {
     pwg_query('INSERT INTO '.CONFIG_TABLE.' (param,value,comment) VALUES ("cryptographp_theme","cryptographp","CryptograPHP theme");');
   }
