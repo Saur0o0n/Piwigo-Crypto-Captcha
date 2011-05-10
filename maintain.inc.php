1931f393a5f4246d4b0ae201225808403eee98a0
mistic100 <mistic@piwigo.org>
2011-05-10 16:35:37 +0000

    add an other (don't forget to test before commit...)
    git-svn-id: http://piwigo.org/svn/extensions/CryptograPHP@10841 68402e56-0260-453c-a942-63ccdbb3a9ee

--- maintain.inc.php
+++ maintain.inc.php
@@ -2,24 +2,12 @@
 function plugin_install()
 {
   global $conf;
-
   
   if (!isset($conf['cryptographp_theme']))
   {
     pwg_query('INSERT INTO '.CONFIG_TABLE.' (param,value,comment) VALUES ("cryptographp_theme","cryptographp","CryptograPHP theme");');
   }
 }
-
-function plugin_activate($id, $version, &$errors)
-{
-  global $conf;
-
-  if (!isset($conf['cryptographp_theme']))
-  {
-    plugin_install();
-  }
-}
-
 
 function plugin_uninstall()
 {
