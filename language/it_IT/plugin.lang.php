5873a8fe76fc32660114b8fcc7cc154d7c23b9dd
riopwg <riopwg@piwigo.org>
2011-06-16 21:40:57 +0000

    Update language IT
    git-svn-id: http://piwigo.org/svn/extensions/CryptograPHP@11400 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/it_IT/plugin.lang.php
+++ language/it_IT/plugin.lang.php
@@ -3,5 +3,8 @@
 $lang['Invalid Captcha'] = 'Captcha non valido';
 $lang['Captcha theme'] = 'Tema del captcha';
 $lang['Antibot test'] = 'Test Antibot';
+$lang['Comments action'] = 'Azione per i commenti';
+$lang['Moderate'] = 'Moderare';
+$lang['No captcha'] = 'Nessun captcha';
 
 ?>
