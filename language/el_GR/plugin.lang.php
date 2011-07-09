2b4b8c621bfc73d7c62bfa9e061da67df5fcde55
ddtddt <ddtddt@piwigo.org>
2011-07-09 11:03:09 +0000

    [extensions] - CryptograPHP - el_GR- Greek - ελληνικά thanks to christos
    git-svn-id: http://piwigo.org/svn/extensions/CryptograPHP@11672 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['Invalid Captcha'] = 'Άκυρο Captcha';
+$lang['Captcha theme'] = 'Θέμα Captcha';
+$lang['Antibot test'] = 'Δοκιμή Antibot';
+$lang['Comments action'] = 'Ενέργεια σχολίου';
+$lang['Moderate'] = 'Μέτριο';
+$lang['No captcha'] = 'Χωρίς  captcha';
+
+?>
