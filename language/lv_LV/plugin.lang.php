bdac9ea05cc3011649f700dd76aaf682ac9b476a
ddtddt <ddtddt@piwigo.org>
2011-07-21 10:08:08 +0000

    [extensions] - CryptograPHP - update LV (Latvian - Latviešu) thanks to Aivars Baldone
    git-svn-id: http://piwigo.org/svn/extensions/CryptograPHP@11798 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/lv_LV/plugin.lang.php
+++ language/lv_LV/plugin.lang.php
@@ -3,5 +3,8 @@
 $lang['Invalid Captcha'] = 'Nederīgs Captcha';
 $lang['Captcha theme'] = 'Captcha tēma';
 $lang['Antibot test'] = 'Antibota (pretrobot) tests';
+$lang['Comments action'] = 'Darbība ar komentāriem';
+$lang['Moderate'] = 'Apmierinošs';
+$lang['No captcha'] = 'Nav captcha';
 
 ?>
