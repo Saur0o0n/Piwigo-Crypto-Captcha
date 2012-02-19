cfea65bca989535df233e5512ed2a37326a22316
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-02-19 22:07:01 +0000

    [CryptoCaptcha] Update hu_HU, thanks to : samli
    git-svn-id: http://piwigo.org/svn/extensions/CryptograPHP@13262 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/hu_HU/plugin.lang.php
+++ language/hu_HU/plugin.lang.php
@@ -4,6 +4,25 @@ $lang['Invalid Captcha'] = 'Érvénytelen Captcha';
 $lang['Captcha theme'] = 'Captcha téma';
 $lang['Comments action'] = 'Hozzászólás műveletek';
 $lang['Moderate'] = 'Moderálás';
-$lang['No captcha'] = 'Nincs captcha';
+$lang['No captcha'] = 'Nem captcha';
 
+$lang['Background color'] = 'Háttérszín';
+$lang['Captcha type'] = 'Captcha típus';
+$lang['Case sensitive'] = 'Nagybetűérzékeny';
+$lang['Code lenght'] = 'Kód hossz';
+$lang['Enter code'] = 'Írja be a kódot';
+$lang['Font'] = 'Betütípus';
+$lang['good value:'] = 'jó érték:';
+$lang['Lines color'] = 'Vonalak színe';
+$lang['Lines density'] = 'Vonalak száma';
+$lang['Noise color'] = 'Zajszín';
+$lang['Noise level'] = 'Zajszint';
+$lang['Perturbation'] = 'Zavarás';
+$lang['Preview'] = 'Előnézet';
+$lang['Random string'] = 'Véletlen karakterlánc';
+$lang['range:'] = 'érték:';
+$lang['Simple equation'] = 'Egyszerű művelet';
+$lang['Solve equation'] = 'Művelet eredménye';
+$lang['Text color'] = 'Szövegszín';
+$lang['Tip: type "random" on a color field to have a random color'] = 'Tipp: A "random" kifejezés használata bármely szín mezőben az adott mező véletlen színét eredményezi.';
 ?>
