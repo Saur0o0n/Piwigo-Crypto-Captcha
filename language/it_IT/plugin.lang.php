d3e6fa3b91e1cae58d16b78625c785fe068fb1ee
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-02-25 09:29:06 +0000

    [CryptoCaptcha] Update it_IT, thanks to : Gdvpixel
    git-svn-id: http://piwigo.org/svn/extensions/CryptograPHP@13332 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/it_IT/plugin.lang.php
+++ language/it_IT/plugin.lang.php
@@ -6,4 +6,21 @@ $lang['Comments action'] = 'Azione per i commenti';
 $lang['Moderate'] = 'Moderare';
 $lang['No captcha'] = 'Nessun captcha';
 
+$lang['Background color'] = 'Colore di sfondo';
+$lang['Captcha type'] = 'Tipo di captcha';
+$lang['Case sensitive'] = 'Distinzione tra maiuscole e minuscole';
+$lang['Code lenght'] = 'Lunghezza codice';
+$lang['Enter code'] = 'Inserisci codice';
+$lang['Font'] = 'Carattere';
+$lang['good value:'] = 'Risultato corretto:';
+$lang['Lines color'] = 'Colore linee';
+$lang['Lines density'] = 'Densità linee';
+$lang['Noise color'] = 'Colore disturbi';
+$lang['Noise level'] = 'Livello disturbi';
+$lang['Preview'] = 'Anteprima';
+$lang['Random string'] = 'Testo casuale';
+$lang['Simple equation'] = 'Operazione semplice';
+$lang['Solve equation'] = 'Risolvi operazione';
+$lang['Text color'] = 'Colore del testo';
+$lang['Tip: type "random" on a color field to have a random color'] = 'Consiglio: scrivi "random" nel campo colore per avere un colore casuale';
 ?>
