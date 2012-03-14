07b41017930b0392b77412ad4ae19ade5c1cc1b0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-03-14 16:59:27 +0000

    [CryptoCaptcha] Update cs_CZ, thanks to : coudy
    git-svn-id: http://piwigo.org/svn/extensions/CryptograPHP@13562 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/cs_CZ/plugin.lang.php
+++ language/cs_CZ/plugin.lang.php
@@ -6,4 +6,23 @@ $lang['Comments action'] = 'Komentáře akce';
 $lang['Moderate'] = 'Moderovat';
 $lang['No captcha'] = 'Bez captcha kódu';
 
+$lang['Background color'] = 'barva pozadí';
+$lang['Captcha type'] = 'typ Captcha kódu';
+$lang['Case sensitive'] = 'text rozlišuje velká a malá písmena';
+$lang['Code lenght'] = 'délka kódu';
+$lang['Enter code'] = 'vlož kód';
+$lang['Font'] = 'typ písma';
+$lang['good value:'] = 'dobrá hodnota:';
+$lang['Lines color'] = 'barva čar';
+$lang['Lines density'] = 'hustota čar';
+$lang['Noise color'] = 'barva šumu';
+$lang['Noise level'] = 'úroveň šumu';
+$lang['Perturbation'] = 'odchylka';
+$lang['Preview'] = 'předběžný náhled';
+$lang['Random string'] = 'náhodný řetězec';
+$lang['range:'] = 'rozsah:';
+$lang['Simple equation'] = 'jednoduchá rovnice';
+$lang['Solve equation'] = 'vyřeš rovnici';
+$lang['Text color'] = 'barva textu';
+$lang['Tip: type "random" on a color field to have a random color'] = 'tip: napsáním textu "random" do políček Barva vygeneruje náhodnou barvu';
 ?>
