e607c6164a80c4a1761f1fdf07ccb5a25942efd8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-03-17 11:28:57 +0000

    [CryptoCaptcha] Update pl_PL, thanks to : Greybrow
    git-svn-id: http://piwigo.org/svn/extensions/CryptograPHP@13587 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/pl_PL/plugin.lang.php
+++ language/pl_PL/plugin.lang.php
@@ -6,4 +6,23 @@ $lang['Invalid Captcha'] = 'Nieprawidłowy kod';
 $lang['Moderate'] = 'Średni';
 $lang['No captcha'] = 'Brak captcha';
 
+$lang['Background color'] = 'Kolor tła';
+$lang['Captcha type'] = 'Typ captcha';
+$lang['Case sensitive'] = 'Czy wielkość liter ma znaczenie';
+$lang['Code lenght'] = 'Długość kodu';
+$lang['Enter code'] = 'Wprowadź kod';
+$lang['Font'] = 'Czcionka';
+$lang['good value:'] = 'wartość prawidłowa:';
+$lang['Lines color'] = 'Kolor linii';
+$lang['Lines density'] = 'Gęstość linii';
+$lang['Noise color'] = 'Kolor szumu';
+$lang['Noise level'] = 'Poziom szumu';
+$lang['Perturbation'] = 'Zakłócenia';
+$lang['Preview'] = 'Podgląd';
+$lang['Random string'] = 'Losowy ciąg znaków';
+$lang['range:'] = 'zakres:';
+$lang['Simple equation'] = 'Proste równanie';
+$lang['Solve equation'] = 'Rozwiąż równanie';
+$lang['Text color'] = 'Kolor tekstu';
+$lang['Tip: type "random" on a color field to have a random color'] = 'Podpowiedź: wpisz "random" w polu koloru, aby otrzymać losowy kolor';
 ?>
