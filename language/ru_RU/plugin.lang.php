940dd36a95630657800dfc1265f6b4de535f9942
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-16 17:44:31 +0000

    [CryptoCaptcha] Update ru_RU, thanks to : marchelly
    git-svn-id: http://piwigo.org/svn/extensions/CryptograPHP@14187 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Background color'] = 'Цвет фона';
+$lang['Captcha theme'] = 'Тема Каптчи';
+$lang['Captcha type'] = 'Тип Каптчи';
+$lang['Case sensitive'] = 'Регистрозависимое';
+$lang['Code lenght'] = 'Длина кода';
+$lang['Comments action'] = 'Комментарии действий';
+$lang['Enter code'] = 'Введите код';
+$lang['Font'] = 'Шрифт';
+$lang['Invalid Captcha'] = 'Неверная Каптча';
+$lang['Lines color'] = 'Цвет линий';
+$lang['Lines density'] = 'Плотность линий';
+$lang['Moderate'] = 'Проверить';
+$lang['No captcha'] = 'Каптча отсутствует';
+$lang['Noise color'] = 'Цветной шум';
+$lang['Noise level'] = 'Уровень шума';
+$lang['Perturbation'] = 'Пертурбация';
+$lang['Preview'] = 'Предварительный просмотр';
+$lang['Random string'] = 'Случайная строка';
+$lang['Simple equation'] = 'Простое уравнение';
+$lang['Solve equation'] = 'Решите уравнение';
+$lang['Text color'] = 'Цвет текста';
+$lang['Tip: type "random" on a color field to have a random color'] = 'Подсказка: введите "random" в поле цвет, чтобы цвет был случайным';
+$lang['good value:'] = 'хорошее значение:';
+$lang['range:'] = 'диапазон:';
+?>
