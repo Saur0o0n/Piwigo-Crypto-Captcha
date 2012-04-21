883a294be144d8af43c4fa90990c2ccceed6fe0a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-21 23:20:19 +0000

    [CryptoCaptcha] Add Deutsch [DE], thanks to : Joergen
    git-svn-id: http://piwigo.org/svn/extensions/CryptograPHP@14254 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Background color'] = 'Hintergrund Farbe';
+$lang['Captcha theme'] = 'Captcha Design';
+$lang['Captcha type'] = 'Captcha Typ';
+$lang['Case sensitive'] = 'Groß-und Kleinschreibung';
+$lang['Code lenght'] = 'Code-Länge';
+$lang['Comments action'] = 'Kommentare Aktion';
+$lang['Enter code'] = 'Code eingeben';
+$lang['Font'] = 'Schriftart';
+$lang['Invalid Captcha'] = 'Unzulässiger Captcha';
+$lang['Lines color'] = 'Linien Farbe';
+$lang['Lines density'] = 'Linien Dichte';
+$lang['Moderate'] = 'Moderate';
+$lang['No captcha'] = 'Kein Captcha';
+$lang['Noise color'] = 'Rauschfarbe';
+$lang['Noise level'] = 'Rauschpegel';
+$lang['Perturbation'] = 'Störung';
+$lang['Preview'] = 'Vorschau';
+$lang['Random string'] = 'Zufällige Zeichenkette';
+$lang['Simple equation'] = 'Einfache Gleichung';
+$lang['Solve equation'] = 'Gleichung lösen';
+$lang['Text color'] = 'Text Farbe';
+$lang['Tip: type "random" on a color field to have a random color'] = 'Tipp: Geben Sie "Zufällig" auf einem Farbfeld ein, um eine Zufalls Farbe zu bekommen';
+$lang['good value:'] = 'guter Wert';
+$lang['range:'] = 'Bereich';
+?>
