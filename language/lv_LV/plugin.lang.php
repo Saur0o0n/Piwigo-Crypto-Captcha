900c454f56f5837acd32c5fc950c1db4f76af100
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-15 11:28:59 +0000

    [cryptocaptcha] Update lv_LV, thanks to : agrisans
    git-svn-id: http://piwigo.org/svn/extensions/CryptograPHP@15719 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/lv_LV/plugin.lang.php
+++ language/lv_LV/plugin.lang.php
@@ -2,9 +2,28 @@
 
 $lang['Invalid Captcha'] = 'Nederīgs Captcha';
 $lang['Captcha theme'] = 'Captcha tēma';
-$lang['Antibot test'] = 'Antibota (pretrobot) tests';
 $lang['Comments action'] = 'Darbība ar komentāriem';
 $lang['Moderate'] = 'Apmierinošs';
 $lang['No captcha'] = 'Nav captcha';
 
+$lang['Background color'] = 'Fona krāsa';
+$lang['Captcha type'] = 'Captcha tips';
+$lang['Case sensitive'] = 'Reģistrjutīgs';
+$lang['Code lenght'] = 'Koda garums';
+$lang['Enter code'] = 'Ievadīt kodu';
+$lang['Font'] = 'Fonts';
+$lang['Lines color'] = 'Līnijas krāsa';
+$lang['Lines density'] = 'Līnijas biezums';
+$lang['Noise color'] = 'Trokšņu krāsa';
+$lang['Noise level'] = 'Trokšņu līmenis';
+$lang['Perturbation'] = 'Perturbācija';
+$lang['Preview'] = 'Priekšskatījums';
+$lang['Random string'] = 'Gadījuma rinda';
+$lang['Simple equation'] = 'Vienkāršs vienādojums';
+$lang['Solve equation'] = 'Atrisināt vienādojumu';
+$lang['Text color'] = 'Teksta krāsa';
+$lang['Tip: type "random" on a color field to have a random color'] = 'Ieteikums: ierakstiet "random" krāsu laukā, lai iegūtu gadījuma krāsu';
+$lang['good value:'] = 'labvērtība:';
+$lang['random'] = 'gadījuma';
+$lang['range:'] = 'diapazons:';
 ?>
