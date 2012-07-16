3f5aa70d08ac7246a4bb5882ee2ffeade438c2ea
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-16 20:29:47 +0000

    [cryptocaptcha] Update lv_LV, thanks to : agrisans
    git-svn-id: http://piwigo.org/svn/extensions/CryptograPHP@16794 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/lv_LV/plugin.lang.php
+++ language/lv_LV/plugin.lang.php
@@ -30,4 +30,5 @@ $lang['Album comments'] = 'Albūma komentāri';
 $lang['Contact form'] = 'Kontaktforma';
 $lang['Guestbook'] = 'Viesu grāmata';
 $lang['Picture comments'] = 'Attēla komentāri';
+$lang['Register form'] = 'Reģistra forma';
 ?>
