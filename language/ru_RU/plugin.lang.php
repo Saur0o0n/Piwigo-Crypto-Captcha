faa4a2fdb87cdf0607aa8468cffcbd25302b6865
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-12-22 18:14:21 +0000

    [cryptocaptcha] Update ru_RU, thanks to : nadusha
    git-svn-id: http://piwigo.org/svn/extensions/CryptograPHP@19529 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/ru_RU/plugin.lang.php
+++ language/ru_RU/plugin.lang.php
@@ -50,4 +50,5 @@ $lang['Contact form'] = 'Контактная форма';
 $lang['Guestbook'] = 'Гостевая книга';
 $lang['Picture comments'] = 'Комментарии к картинкам';
 $lang['Register form'] = 'Форма регистрации';
+$lang['Button color'] = 'Цвет кнопки';
 ?>
