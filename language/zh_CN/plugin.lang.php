e11026d2890b23d4a12c548193ce6ae34a26faac
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-27 19:01:03 +0000

    [cryptocaptcha] Add zh_CN, thanks to : dennisyan
    git-svn-id: http://piwigo.org/svn/extensions/CryptograPHP@20426 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Register form'] = '注册界面';
+$lang['Solve equation'] = '填写计算结果';
+$lang['Contact form'] = 'ContactForm 插件';
+$lang['Guestbook'] = 'Guestbook 插件';
+$lang['Comments action'] = 'Comments action';
+$lang['Moderate'] = 'Moderate';
+$lang['Case sensitive'] = 'Case sensitive';
+$lang['Activate on'] = '在何处启用';
+$lang['Button color'] = '刷新按钮颜色';
+$lang['Code lenght'] = '字符个数（length）';
+$lang['Picture comments'] = '图片评论';
+$lang['Preview'] = '预览';
+$lang['Random string'] = '随机字符串';
+$lang['Simple equation'] = '简单算术';
+$lang['Text color'] = '字体颜色';
+$lang['Tip: type "random" on a color field to have a random color'] = '提示：在颜色框键入 "random" 可设为随机颜色';
+$lang['good value:'] = '推荐值：';
+$lang['random'] = '随机';
+$lang['range:'] = '范围：';
+$lang['Album comments'] = '相册评论';
+$lang['Background color'] = '背景颜色';
+$lang['Captcha theme'] = '验证码主题';
+$lang['Captcha type'] = '验证码类型';
+$lang['Enter code'] = '输入字符';
+$lang['Font'] = '字体';
+$lang['Invalid Captcha'] = '验证码无效';
+$lang['Lines color'] = '条纹颜色';
+$lang['Lines density'] = '条纹密度';
+$lang['Noise color'] = '噪点颜色';
+$lang['Noise level'] = '噪点密度';
+$lang['Perturbation'] = '扭曲度';
+?>
