144e48432b81834211591d9746f18a15d91d8ca5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-03 18:25:05 +0000

    [cryptocaptcha] Add pt_BR, thanks to : flaviove
    git-svn-id: http://piwigo.org/svn/extensions/CryptograPHP@21125 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Picture comments'] = 'Comentários de imagens';
+$lang['Preview'] = 'Previsão';
+$lang['Random string'] = 'Seqüência aleatória';
+$lang['Register form'] = 'Formulário de registro';
+$lang['Simple equation'] = 'Equação simples';
+$lang['Solve equation'] = 'resolver equação';
+$lang['Text color'] = 'Cor do testo';
+$lang['Tip: type "random" on a color field to have a random color'] = 'Dica: digite "aleatório" em um campo de cor para ter uma cor aleatória';
+$lang['good value:'] = 'bom valor:';
+$lang['random'] = 'aleatório';
+$lang['range:'] = 'intervalo:';
+$lang['Activate on'] = 'Ativar em';
+$lang['Album comments'] = 'Comentários de album';
+$lang['Background color'] = 'Cor de fundo';
+$lang['Button color'] = 'Cor do botão';
+$lang['Captcha theme'] = 'Tema captcha';
+$lang['Captcha type'] = 'Tipo de captcha';
+$lang['Case sensitive'] = 'Detectar Maiúsculas de Minúsculas';
+$lang['Code lenght'] = 'Comprimento do código';
+$lang['Comments action'] = 'Ação comentários';
+$lang['Contact form'] = 'Formulário de contato';
+$lang['Enter code'] = 'Digite o código';
+$lang['Font'] = 'Fonte';
+$lang['Guestbook'] = 'Livro de Visitas';
+$lang['Invalid Captcha'] = 'Captcha Inválida';
+$lang['Lines color'] = 'Cor das linhas';
+$lang['Lines density'] = 'Densidade das linhas';
+$lang['Moderate'] = 'Moderada';
+$lang['Noise color'] = 'Cor barulhenta';
+$lang['Noise level'] = 'Nível barulhento';
+$lang['Perturbation'] = 'Pertubação';
+?>
