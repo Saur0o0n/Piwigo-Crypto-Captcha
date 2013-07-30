9c40e2392ab155a21a0c13251e87a0ae1d27688c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-07-30 11:05:18 +0000

    [cryptocaptcha] Add zh_TW, thanks to : azq1231
    git-svn-id: http://piwigo.org/svn/extensions/CryptograPHP@24074 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Comments action'] = '評論行動';
+$lang['Noise level'] = '雜訊等級';
+$lang['Perturbation'] = '扭曲度';
+$lang['Noise color'] = '雜訊顏色';
+$lang['Moderate'] = '中等';
+$lang['Lines density'] = '線的密度';
+$lang['Lines color'] = '線的顏色';
+$lang['Picture comments'] = '圖片評論';
+$lang['Solve equation'] = '填寫計算結果';
+$lang['Simple equation'] = '簡單算術';
+$lang['Register form'] = '註冊表格';
+$lang['Random string'] = '隨機字串';
+$lang['Preview'] = '預覽';
+$lang['Tip: type "random" on a color field to have a random color'] = '提示：在顏色框鍵入“隨機”，可設為隨機顏色';
+$lang['Text color'] = '文字顏色';
+$lang['good value:'] = '推薦值：';
+$lang['random'] = '隨機';
+$lang['range:'] = '範圍：';
+$lang['Invalid Captcha'] = '無效的驗證碼';
+$lang['Guestbook'] = '訪客留言本';
+$lang['Font'] = '字型';
+$lang['Enter code'] = '輸入驗證碼';
+$lang['Contact form'] = '聯繫表單';
+$lang['Code lenght'] = '驗證碼長度';
+$lang['Case sensitive'] = '區分大小寫';
+$lang['Captcha type'] = '驗證碼類型';
+$lang['Captcha theme'] = '驗證碼主題';
+$lang['Button color'] = '按鈕顏色';
+$lang['Background color'] = '背景顏色';
+$lang['Album comments'] = '相冊評論';
+$lang['Activate on'] = '在何處啟用';
+?>
