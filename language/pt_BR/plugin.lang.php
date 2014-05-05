56826694b8397592a88e247277da2ad09be6ec17
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-05-05 09:56:37 +0000

    [cryptocaptcha] Update pt_BR, thanks to : flaviove
    git-svn-id: http://piwigo.org/svn/extensions/CryptograPHP@28352 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/pt_BR/plugin.lang.php
+++ language/pt_BR/plugin.lang.php
@@ -57,4 +57,4 @@ $lang['Background'] = 'Fundo';
 $lang['Background image'] = 'Imagem de fundo';
 $lang['Color'] = 'Cor';
 $lang['Image'] = 'Imagem';
-?>
+$lang['Only for unauthenticated users'] = 'Somente para usuários autenticados';
