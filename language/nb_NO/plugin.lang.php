d306bfe0542a75a24595b690027fd00912880fed
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-10-14 19:37:32 +0000

    [cryptocaptcha] Update nb_NO, thanks to : paulen
    git-svn-id: http://piwigo.org/svn/extensions/CryptograPHP@30064 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nb_NO/plugin.lang.php
@@ -0,0 +1,60 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Moderate'] = 'Moderat';
+$lang['Noise color'] = 'Fargestøy';
+$lang['Noise level'] = 'Støynivå';
+$lang['Nowhere'] = 'Ingen steder';
+$lang['Only for unauthenticated users'] = 'Bare for godkjente brukere';
+$lang['Perturbation'] = 'Forstyrrelse';
+$lang['Picture comments'] = 'Bilde kommentar';
+$lang['Preview'] = 'Forhåndsvisning';
+$lang['Random string'] = 'Tilfeldig tråd';
+$lang['Register form'] = 'Registrerings skjema';
+$lang['Simple equation'] = 'Enkel ligning';
+$lang['Solve equation'] = 'Løs ligningen';
+$lang['Text color'] = 'Tekstfarge';
+$lang['Tip: type "random" on a color field to have a random color'] = 'Tips: skriv "tilfeldig" på en farge-felt for å få en tilfeldig farge';
+$lang['We detected that EasyCaptcha plugin is available on your gallery. Both plugins can be used at the same time, but you should not under any circumstances activate both of them on the same page.'] = 'Vi har oppdaget at EasyCaptcha programtillegget er tilgjengelig i ditt galleri. Begge programtilleggene kan brukes på samme tid, men du bør ikke under noen omstendigheter aktivere dem begge på samme side.';
+$lang['good value:'] = 'God verdi:';
+$lang['random'] = 'Tilfeldig';
+$lang['range:'] = 'Rekkefølge:';
+$lang['Activate on'] = 'Aktiver på';
+$lang['Album comments'] = 'Album kommentarer';
+$lang['Background'] = 'Bakgrunn';
+$lang['Background color'] = 'Bakgrunnsfarge';
+$lang['Background image'] = 'Bakgrunnsbilde';
+$lang['Button color'] = 'Knappfarge';
+$lang['Captcha theme'] = 'Captcha tema';
+$lang['Captcha type'] = 'Captcha type';
+$lang['Case sensitive'] = 'Sjeldne mellom store og små bokstaver';
+$lang['Code lenght'] = 'Kodelengde';
+$lang['Color'] = 'Farge';
+$lang['Comments action'] = 'Handlings kommentarer ';
+$lang['Contact form'] = 'Kontakt skjema';
+$lang['Enter code'] = 'Legg til kode';
+$lang['Font'] = 'Skrift';
+$lang['Guestbook'] = 'Gjestebok';
+$lang['Image'] = 'Bilde';
+$lang['Invalid Captcha'] = 'Ugyldig Captcha';
+$lang['Lines color'] = 'Linjefarge';
+$lang['Lines density'] = 'Linje tetthet';
