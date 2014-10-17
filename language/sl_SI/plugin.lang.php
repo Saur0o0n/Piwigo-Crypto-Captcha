9659531b85855dc9c558b6a5f962eada324a6d5c
ddtddt <ddtddt@piwigo.org>
2014-10-17 16:47:48 +0000

    [extensions] - Crypto -> Add sl_SI - Slovenia - Slovenšcina - Thanks to MarkoAm
    git-svn-id: http://piwigo.org/svn/extensions/CryptograPHP@30180 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+
+$lang['Invalid Captcha'] = 'Neveljavna Captcha';
+$lang['Enter code'] = 'Prepiši znake';
+$lang['Solve equation'] = 'Rešitev enčbe';
+$lang['Button color'] = 'Barva gumba';
+$lang['Comments action'] = 'Dejanje na komentarjih';
+$lang['Moderate'] = 'Moderiranje';
+$lang['Captcha type'] = 'Captcha tip';
+$lang['Random string'] = 'Naključen niz';
+$lang['Simple equation'] = 'Preprosta enačba';
+$lang['Case sensitive'] = 'Razlikuj velike - male črke';
+$lang['Code lenght'] = 'število znakov';
+$lang['good value:'] = 'dobra vrednost:';
+$lang['Captcha theme'] = 'Captcha tema';
+$lang['Activate on'] = 'Aktiviraj na';
+$lang['Picture comments'] = 'Komentarji slik';
+$lang['Album comments'] = 'Komentarji Albumov';
+$lang['Register form'] = 'Registracijski obrazec';
+$lang['Contact form'] = 'Kontaktni obrazec';
+$lang['Guestbook'] = 'Knjiga gostov';
+$lang['Perturbation'] = 'Zameglitev';
+$lang['range:'] = 'razpon:';
+$lang['random'] = 'naključno';
+$lang['Background color'] = 'Barva ozadja';
+$lang['Text color'] = 'Barva besedila';
+$lang['Lines density'] = 'Gostota črt';
+$lang['Lines color'] = 'Barva črt';
+$lang['Noise level'] = 'Nivo šuma';
+$lang['Noise color'] = 'Barva šuma';
+$lang['Font'] = 'Fonti';
+$lang['Preview'] = 'Predogled';
+$lang['Background'] = 'Ozadje';
+$lang['Background image'] = 'Slika ozadja';
+$lang['Color'] = 'Barva';
+$lang['Image'] = 'Slika';
+$lang['Tip: type "random" on a color field to have a random color'] = 'Namig: tip "naključno" na barvnem polju za naključno barvo';
+$lang['We detected that EasyCaptcha plugin is available on your gallery. Both plugins can be used at the same time, but you should not under any circumstances activate both of them on the same page.'] = 'Zaznali smo da je v vaši galeriji na voljo vtičnik EasyCaptcha. Oba vtičnika se lahko uporabljata vendar pa pod nobenim pogojem ne smete aktivirati oba vtičnika na isti strani.';
+$lang['Only for unauthenticated users'] = 'Samo za nepreverjene uporabnike';
+
