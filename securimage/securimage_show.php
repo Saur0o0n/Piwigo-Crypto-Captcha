4488471082ed0f2be31137cb36a475d44c9d2785
Adrian Siemieniak <sauron@sauron.ordugh.org>
2017-02-16 00:16:31 +0100

    update Securimage to 3.6.4, without update captch plugin does not work

--- securimage/securimage_show.php
+++ securimage/securimage_show.php
@@ -1,20 +1,21 @@
 <?php
+
 /**
- * Project:     Securimage: A PHP class for creating and managing form CAPTCHA images
- * File:        securimage_show.php
+ * Project:     Securimage: A PHP class for creating and managing form CAPTCHA images<br />
+ * File:        securimage_show.php<br />
  *
- * Copyright (c) 2011, Drew Phillips
+ * Copyright (c) 2013, Drew Phillips
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
- * 
+ *
  *  - Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  *  - Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -28,73 +29,51 @@
  * POSSIBILITY OF SUCH DAMAGE.
  *
  * Any modifications to the library should be indicated clearly in the source code
- * to inform users that the changes are not a part of the original software.
+ * to inform users that the changes are not a part of the original software.<br /><br />
  *
- * If you found this script useful, please take a quick moment to rate it.
+ * If you found this script useful, please take a quick moment to rate it.<br />
  * http://www.hotscripts.com/rate/49400.html  Thanks.
  *
  * @link http://www.phpcaptcha.org Securimage PHP CAPTCHA
  * @link http://www.phpcaptcha.org/latest.zip Download Latest Version
  * @link http://www.phpcaptcha.org/Securimage_Docs/ Online Documentation
- * @copyright 2011 Drew Phillips
+ * @copyright 2013 Drew Phillips
  * @author Drew Phillips <drew@drew-phillips.com>
- * @version 3.0 (October 2011)
+ * @version 3.5.2 (Feb 15, 2014)
  * @package Securimage
  *
  */
 
-define('PHPWG_ROOT_PATH','../../../');
-include_once(PHPWG_ROOT_PATH.'include/common.inc.php');
-
-
-// randomize colors
-function randomColor()
-{
-  mt_srand((double)microtime()*1000000);
-  $c = null;
-  while(strlen($c)<6)
-  {
-      $c .= sprintf("%02X", mt_rand(0, 255));
-  }
-  return $c;
-}
-
-foreach (array('bg_color','text_color','line_color','noise_color') as $color)
-{
-  if ($conf['cryptographp'][$color] == 'random') $conf['cryptographp'][$color] = randomColor();
-}
-
+// Remove the "//" from the following line for debugging problems
+// error_reporting(E_ALL); ini_set('display_errors', 1);
 
 require_once dirname(__FILE__) . '/securimage.php';
-$img = new securimage();
 
-$img->ttf_file        = './fonts/'.$conf['cryptographp']['ttf_file'].'.ttf';
-$img->captcha_type    = ($conf['cryptographp']['captcha_type'] == 'string') ? Securimage::SI_CAPTCHA_STRING : Securimage::SI_CAPTCHA_MATHEMATIC;
-// $img->case_sensitive  = get_boolean($conf['cryptographp']['case_sensitive']);
-$img->image_width     = $conf['cryptographp']['width']; 
-$img->image_height    = $conf['cryptographp']['height'];
-$img->perturbation    = $conf['cryptographp']['perturbation'];
-$img->text_color      = new Securimage_Color('#'.$conf['cryptographp']['text_color']); 
-$img->num_lines       = $conf['cryptographp']['num_lines'];
-$img->line_color      = new Securimage_Color('#'.$conf['cryptographp']['line_color']);
-$img->noise_level     = $conf['cryptographp']['noise_level'];
-$img->noise_color     = new Securimage_Color('#'.$conf['cryptographp']['noise_color']);
-$img->code_length     = $conf['cryptographp']['code_length'];
+$img = new Securimage();
 
-if ($conf['cryptographp']['background'] == 'image')
-{
-  if ($conf['cryptographp']['bg_image'] == 'random')
-  {
-    $img->background_directory = realpath(CRYPTO_PATH . 'securimage/backgrounds/');
-    $img->show();
-  }
-  else
-  {
-    $img->show(realpath(CRYPTO_PATH . 'securimage/backgrounds/' . $conf['cryptographp']['bg_image']));
-  }
-}
-else
-{
-  $img->image_bg_color  = new Securimage_Color('#'.$conf['cryptographp']['bg_color']);
-  $img->show();
-}
+// You can customize the image by making changes below, some examples are included - remove the "//" to uncomment
+
+//$img->ttf_file        = './Quiff.ttf';
+//$img->captcha_type    = Securimage::SI_CAPTCHA_MATHEMATIC; // show a simple math problem instead of text
+//$img->case_sensitive  = true;                              // true to use case sensitve codes - not recommended
+//$img->image_height    = 90;                                // height in pixels of the image
+//$img->image_width     = $img->image_height * M_E;          // a good formula for image size based on the height
+//$img->perturbation    = .75;                               // 1.0 = high distortion, higher numbers = more distortion
+//$img->image_bg_color  = new Securimage_Color("#0099CC");   // image background color
+//$img->text_color      = new Securimage_Color("#EAEAEA");   // captcha text color
+//$img->num_lines       = 8;                                 // how many lines to draw over the image
+//$img->line_color      = new Securimage_Color("#0000CC");   // color of lines over the image
+//$img->image_type      = SI_IMAGE_JPEG;                     // render as a jpeg image
+//$img->signature_color = new Securimage_Color(rand(0, 64),
+//                                             rand(64, 128),
+//                                             rand(128, 255));  // random signature color
+
+// see securimage.php for more options that can be set
+
+// set namespace if supplied to script via HTTP GET
+if (!empty($_GET['namespace'])) $img->setNamespace($_GET['namespace']);
+
+
+$img->show();  // outputs the image and content headers to the browser
+// alternate use:
+// $img->show('/path/to/background_image.jpg');
